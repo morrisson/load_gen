0d578fdc38db7a03716332fe3ae894c81342fe46
Daisuke Morita <daisuke.morita@ntti3.io>
2019-01-11 20:27:26 -0800

    Add anonymized const

---
+++ common/const.go
@@ -0,0 +1,10 @@
+package common
+
+const (
+	GohanEndpointURL    = "https://<host>/gohan"
+	KeystoneEndpointURL = "https://<host>/keystone/v2.0"
+
+	GohanUser     = "<user>"
+	GohanPassword = "<password>"
+	GohanTenant   = "<tenant>"
+)
